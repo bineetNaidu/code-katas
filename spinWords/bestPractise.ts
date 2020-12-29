fcfade0d4301fa30069ae02217cfb044f9b44caa
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-30 00:02:30 +0530

    completed "spinWords" kata (6 kyu)

---
+++ spinWords/bestPractise.ts
@@ -0,0 +1,8 @@
+export class Kata {
+  static spinWords(words: string) {
+    return words
+      .split(' ') //? Split words up
+      .map((w: string) => (w.length >= 5 ? w.split('').reverse().join('') : w))
+      .join(' '); //? Put them back together
+  }
+}
---
+++ spinWords/index.ts
@@ -0,0 +1,32 @@
+/*
+ * Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter
+ * words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be
+ * included only when more than one word is present.
+ */
+/*
+ * Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This
+ * is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+ */
+
+export class Kata {
+  static spinWords(words: string) {
+    const splitWords = words.split(' ');
+    let spinnedWord: string[] = [];
+
+    if (splitWords.length) {
+      for (let word of splitWords) {
+        const lenghtOfWord = word.length;
+
+        if (lenghtOfWord >= 5) {
+          const splitWordsArr = word.split('');
+          spinnedWord.push(splitWordsArr.reverse().join(''));
+        } else {
+          spinnedWord.push(word);
+        }
+      }
+    }
+    return spinnedWord.join(' ');
+  }
+}
+
+console.log(Kata.spinWords('Hey fellow warriors'));
