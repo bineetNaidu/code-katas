3cdb80041d786dd6c88f01c180880a94322f84de
bineetNaidu <bineet.s.naidu@gmail.com>
2020-12-30 15:53:07 +0530

    completed "Reserve a word" kata (7 kyu)

---
+++ ReverseWords/index.ts
@@ -0,0 +1,20 @@
+/*
+ * Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should
+ * be retained.
+ *
+ * Example
+ *  "This is an example!" ==> "sihT si na !elpmaxe"
+ *  "double  spaces"      ==> "elbuod  secaps"
+ */
+
+export function reverseWords(str: string): string {
+  //TODO your code here
+  return str
+    .split(' ')
+    .map((s) => s.split('').reverse().join(''))
+    .join(' ');
+}
+
+const result = reverseWords('This is an example!');
+
+console.log(result);
