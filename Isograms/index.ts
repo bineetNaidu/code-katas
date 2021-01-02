28235dc916384e5ff32d4b6dfb6cf179acfd33bf
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-02 14:23:30 +0530

    complete "Isograms" kata (7 kyu)

---
+++ Isograms/index.ts
@@ -0,0 +1,20 @@
+/*
+ * An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+ * Implement a function that determines
+ * whether a string that contains only letters is an isogram.
+ * Assume the empty string is an isogram. Ignore letter case.
+ *
+ * URL: https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/typescript
+ */
+
+export function isIsogram(str: string) {
+  const strArr = str.toLowerCase().split('');
+  const uniqueSet = new Set(str.toLowerCase());
+  let uniqueArr: string[] = [];
+  uniqueSet.forEach((s) => uniqueArr.push(s));
+
+  return JSON.stringify(strArr) === JSON.stringify(uniqueArr);
+}
+
+const res = isIsogram('Dermatoglyphics');
+console.log(res);
