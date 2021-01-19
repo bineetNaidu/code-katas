4f1ac019bd5ed1dec6d16fff08634671ae7e14c1
bineetNaidu <bineet.s.naidu@gmail.com>
2021-01-19 23:48:15 +0530

    ❇️ complete Split String (6 kyu) Kata

---
+++ SplitStrings/index.ts
@@ -0,0 +1,37 @@
+/**
+ * Complete the solution so that it splits the string into pairs of two characters.
+ * If the string contains an odd number of characters
+ * then it should replace the missing second character of the final pair with an underscore ('_').
+ *
+ * Example
+ *  splitStrings('abc') // should return [ 'ab' , 'c_' ]
+ *  splitStrings('abcdef') // should return [ 'ab' , 'cd' , 'ef ]
+ */
+
+function splitStrings(str: string): string[] {
+  const isEven = str.length % 2 === 0;
+  let result = [];
+  if (isEven) {
+    for (let i = 0; i < str.length; i++) {
+      const element = str[i];
+      const isOdd = i % 2 !== 0;
+      if (!isOdd) {
+        result.push(element + str[i + 1]);
+      }
+    }
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      const element = str[i];
+      const isOdd = i % 2 !== 0;
+      if (!isOdd) {
+        result.push(element + str[i + 1]);
+      }
+    }
+    const dangleString = result[result.length - 1].replace('undefined', '_');
+    result.pop();
+    result.push(dangleString);
+  }
+  return result;
+}
+
+console.log(splitStrings('abc'));
